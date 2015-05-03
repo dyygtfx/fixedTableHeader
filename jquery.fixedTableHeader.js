3a9b460b536d30b3d4c9fa3007e64295e8ed9622
Go7hic <gtfx0209@qq.com>
2015-05-03 20:04:05 +0800

    Create jquery.fixedTableHeader.js

---
+++ jquery.fixedTableHeader.js
@@ -0,0 +1,63 @@
+;(function() {
+    $.fn.fixedTableHeader = function(options) {
+        var defaults = {
+
+        }
+        var settings = $.extend({},defaults,options);
+        return this.each(function(index){
+            var table = $(this);
+            var fixedheader = $('<div class="header-fixed"></div>');
+            var tableOffset = table.offset().top;
+            var tableleft = table.offset().left;
+            var tablewidth = table.width();
+            var tableheight = table.height();
+
+            if($('thead',table).length < 1) {
+                if($('th',table).length > 0){
+                    $('th',table).eq(0).parent().wrap('<thead class="theader"></thead>');
+                    $('.theader',table).prependTo(table);
+                }
+
+                else $('tr',table).eq(0).wrap('<thead></thead>');
+            }
+
+            var $header = $("thead", table).clone();
+            var newTable = $('<table class="'+table.attr('class')+'"></table>');
+            $header.appendTo(newTable);
+            newTable.css('margin','0');
+
+            fixedheader.css({
+                'position':'fixed',
+                'top':'0px',
+                'display':'none',
+                'left':tableleft+'px',
+                'width':tablewidth+2+'px',
+                'z-index': '103'
+            });
+            var $fixedHeader = fixedheader.append(newTable);
+
+            table.find('th').each(function(index, valuee){
+                console.log($(this).width()+'px');
+                $header.find('th').eq(index).css('width',$(this).outerWidth()+ 'px');
+                // 如果页面布局是 box-sizing: border-box;则用 .outerWith()(包括border,padding)，否则用 .with()(就是元素内容宽度);
+            });
+
+            $(window).on("scroll", function() {
+                var offset = $(this).scrollTop();
+                tableOffset = table.offset().top;
+                tablewidth = table.width();
+                tableheight = table.height();
+                if (offset >= tableOffset && $fixedHeader.is(":hidden") && offset < tableOffset+tableheight) {
+                    fixedheader.appendTo('body');
+                    $fixedHeader.fadeIn(100);
+                    table.addClass('stuck');
+                }
+                else if (offset < tableOffset || offset > tableOffset+tableheight-30) {
+                    $fixedHeader.fadeOut(150);
+                    table.removeClass('stuck');
+                }
+            });
+
+        });
+    }
+})
